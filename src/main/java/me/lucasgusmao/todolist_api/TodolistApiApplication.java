d4edc440815fd44666e8b99c0152b5d2d2a54a46
Lucas Gusmão <lucasgusmaobaptista@gmail.com>
2024-06-10 11:12:53 -0300

    first commit

---
+++ src/main/java/me/lucasgusmao/todolist_api/TodolistApiApplication.java
@@ -0,0 +1,13 @@
+package me.lucasgusmao.todolist_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/lucasgusmao/todolist_api/TodolistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lucasgusmao.todolist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
